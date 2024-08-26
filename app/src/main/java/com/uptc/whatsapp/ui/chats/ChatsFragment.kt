2251ea30620423ef690bc99e79afec17ad870815
Lordeveraxe <santiago.ojedatorres@gmail.com>
2024-08-26 00:09:18 -0500

    Chats layout fixed

--- app/src/main/java/com/uptc/whatsapp/ui/chats/ChatsFragment.kt
+++ app/src/main/java/com/uptc/whatsapp/ui/chats/ChatsFragment.kt
@@ -4,9 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import android.widget.TextView
-import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.constraintlayout.widget.ConstraintSet
 import androidx.fragment.app.Fragment
 import com.uptc.whatsapp.R
 import com.uptc.whatsapp.databinding.FragmentChatsBinding
@@ -16,7 +15,7 @@ class ChatsFragment : Fragment() {
     private var _binding: FragmentChatsBinding? = null
     private val binding get() = _binding!!
 
-    private val numberOfChats = 5
+    private val numberOfChats = 15  // Number of dynamic chat items to create
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -29,26 +28,15 @@ class ChatsFragment : Fragment() {
     }
 
     private fun addChatsDynamically() {
-        var prevViewId = ConstraintSet.PARENT_ID
+        val linearLayoutForChats = binding.linearLayoutForChats
 
         for (i in 1..numberOfChats) {
-            val chatView = LayoutInflater.from(context).inflate(R.layout.person, null, false).apply {
-                id = View.generateViewId()
+            val chatView = LayoutInflater.from(context).inflate(R.layout.person, linearLayoutForChats, false).apply {
                 findViewById<TextView>(R.id.textView_chat_name).text = "Chat Name $i"
                 findViewById<TextView>(R.id.textView_last_message).text = "Last message preview $i"
-                findViewById<TextView>(R.id.textView_timestamp).text = "${10 + i}:00 PM"
+                findViewById<TextView>(R.id.textView_timestamp).text = "${i}/10/2024"
             }
-
-            binding.constraintLayout.addView(chatView)
-
-            val set = ConstraintSet()
-            set.clone(binding.constraintLayout)
-            set.connect(chatView.id, ConstraintSet.START, ConstraintSet.PARENT_ID, ConstraintSet.START, 0)
-            set.connect(chatView.id, ConstraintSet.END, ConstraintSet.PARENT_ID, ConstraintSet.END, 0)
-            set.connect(chatView.id, ConstraintSet.TOP, prevViewId, if (i == 1) ConstraintSet.TOP else ConstraintSet.BOTTOM, 8)
-            set.applyTo(binding.constraintLayout)
-
-            prevViewId = chatView.id
+            linearLayoutForChats.addView(chatView)
         }
     }
 
